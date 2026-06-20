955bc245ae1bf8bf5d4f24336313c71aadd41a91
adityasha56365 <adityasharma346t@gmail.com>
2026-06-20 21:10:42 +0530

    my changes

--- Greedy Algorithm problem/ActivitySelection.java
+++ Greedy Algorithm problem/ActivitySelection.java
@@ -1,4 +1,4 @@
-public class Main {
+public class ActivitySelection {
     public static void main(String[] args) {
         int start[] = {1, 3, 0, 5, 8, 5};
         int end[]   = {2, 4, 6, 7, 9, 9};
